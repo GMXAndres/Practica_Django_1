da6f4b5f8048aae082d3ad407dd8b8babba51f9e
GMXAndres <gmxandres11@gmail.com>
2023-01-25 19:38:15 -0500

    guarda cat

--- prueba/libros/models.py
+++ prueba/libros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from autores.models import Autor
+from categorias.models import Categoria
 
-# Create your models here.
+#Modelo para la entidad Libros
+class Libro(models.Model):
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=500)
+    fecha_pub = models.DateTimeField()
+    fecha_creacion = models.DateTimeField(auto_now=True)
--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autores.apps.AutoresConfig',
+    'categorias.apps.CategoriasConfig',
+    'libros.apps.LibrosConfig',
 ]
 
 MIDDLEWARE = [
